bc56d1bc443cc491659b114924b29f6dca717c23
Gabriel <gabriel.profissional.dev@gmail.com>
2022-09-01 10:53:39 -0300

    calculator created

---
+++ calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace calculator
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Calculator");
+            Console.WriteLine("Please, choice one that options at down:");
+            Console.WriteLine("1 - Sum:");
+            Console.WriteLine("2 - Subtraction:");
+            Console.WriteLine("3 - Multiplication:");
+            Console.WriteLine("4 - Division:");
+            Console.WriteLine("5 - Potentiation:");
+            Console.WriteLine("6 - Square Root:");
+            Console.WriteLine("7 - Cubic Root:");
+            Console.WriteLine("0 - Exit:");
+            int opcao = int.Parse(Console.ReadLine());
+        }
+    }
+}
+
+
